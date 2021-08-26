ea482bb8bf4ba08c1189c41c916da5376a327f7a
remkoduursma <remko@shintolabs.nl>
2021-08-26 14:03:50 +0200

    install shinto r packages

---
+++ R/install_shinto_r_packages.R
@@ -0,0 +1,13 @@
+#' Install (all) Shinto Labs R packages
+#' @param what Which packages to install. Defaults to most common ones.
+install_shinto_rpackages <- function(what = c("shintobag","shintoshiny",
+                                              "shintoui","shinyfilterset","shintodashboard",
+                                              "shintousers","shinysavedom","shintopostcode")){
+  
+  
+  for(p in what){
+    remotes::install_github(paste0("moturoa/",p), upgrade = "never", quiet = TRUE)
+    print(paste(p, ": done installing / no updates found."))
+  }
+  
+}
