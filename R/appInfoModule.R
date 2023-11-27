24d88131147bcef53fa7f2709245ce5c876a0877
WesleyBrants <wesley@shintolabs.nl>
2023-11-27 10:59:38 +0100

    app version op nieuwe manier en oude manier zichbaar in appinfoui

--- R/appInfoModule.R
+++ R/appInfoModule.R
@@ -1,18 +1,18 @@
 #' Content for dropdown with app information
 #' @details A shiny module to generate the dropdown with application information such as
-#' publication date, git version, name, etc. 
+#' publication date, git version, name, etc.
 #' @param id Shiny input id
 #' @param info Optional - if NULL an object returned by \code{\link{read_application_info}}.
 #' @rdname appInfoModule
 #' @export
 appInfoUI <- function(id,
                       info = NULL
-                      ){
+){
 
   if(is.null(info)){
     info <- read_application_info()
   }
-  
+
   ns <- NS(id)
 
   info_row <- function(label, content){
@@ -27,30 +27,36 @@ appInfoUI <- function(id,
 
   }
 
+  if(info$version == ""){
+    app_version <- info$this_version$appversion
+  } else {
+    app_version <- info$version
+  }
+
   release_date <- info$rsconnect$timestamp
-  
+
   if(is.null(release_date)){
     release_date <- info$this_version$appversiondate
   } else {
     release_date <- format(as.Date(as.POSIXct(info$rsconnect$timestamp)))
   }
-    
+
   git_ver <- info$rsconnect$git$sha
   if(is.null(git_ver)){
     git_ver <- info$git$sha
   }
 
   tags$div(
-    tags$img(src = "assets/logo/logoshintolabs.png", 
+    tags$img(src = "assets/logo/logoshintolabs.png",
              width = "100px"),
-    tags$div(info$this_version$appname, 
+    tags$div(info$this_version$appname,
              style = glue::glue("font-size: 1.3em;",
                                 "font-weight: 500;",
                                 "padding-bottom: 30px;",
                                 "padding-top: 20px;")),
 
     tags$table(style = "width: 100%;",
-               info_row("Versie", info$version),
+               info_row("Versie", app_version),
                info_row("Gepubliceerd", release_date),
                info_row("Git", git_ver)
     ),
@@ -67,7 +73,7 @@ appInfoModule <- function(input, output, session){
 
   get_navigator_info(session)
   nav <- reactive(input$navigatorInfo)
-  
+
   output$ui_browser_info <- renderUI({
     bi <- nav()
     req(bi)
@@ -88,7 +94,7 @@ appInfoModule <- function(input, output, session){
   })
 
 
-return(nav)
+  return(nav)
 }
 
 
